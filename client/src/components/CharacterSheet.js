1c7dae63e70107968dbe28a73a147885afccbe60
pupocalypse <64297538+pupocalypse@users.noreply.github.com>
2020-09-17 01:13:27 -0700

    more styling for characters page: rings, skills table

--- client/src/components/CharacterSheet.js
+++ client/src/components/CharacterSheet.js
@@ -66,76 +66,281 @@ const CharacterSheet = () => {
         </div>
       </section>
 
+      <section className="character-sheet__status-container"></section>
+
       <section className="character-sheet__rings-container">
         <div className="character-sheet__all-rings-container">
-          <img src={ringsImg} alt="" className="character-sheet__rings-image" />
+          <div className="character-sheet__rings-image">
+            {/* element rings */}
+            <div className="character-sheet__ring-rank-container earth">
+              <h3 className="character-sheet__trait-rank">3</h3>
+            </div>
+            <div className="character-sheet__ring-rank-container air">
+              <h3 className="character-sheet__trait-rank">2</h3>
+            </div>
+            <div className="character-sheet__ring-rank-container water">
+              <h3 className="character-sheet__trait-rank">2</h3>
+            </div>
+            <div className="character-sheet__ring-rank-container fire">
+              <h3 className="character-sheet__trait-rank">3</h3>
+            </div>
+            <div className="character-sheet__ring-rank-container void">
+              <h3 className="character-sheet__trait-rank">2</h3>
+            </div>
+
+            {/* trait rings */}
+            <div className="character-sheet__ring-rank-container stamina">
+              <h3 className="character-sheet__trait-rank">3</h3>
+              <p className="character-sheet__trait-text stamina-text">
+                Stamina
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container willpower">
+              <h3 className="character-sheet__trait-rank">3</h3>
+              <p className="character-sheet__trait-text willpower-text">
+                Willpower
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container strength">
+              <h3 className="character-sheet__trait-rank">2</h3>
+              <p className="character-sheet__trait-text strength-text">
+                Strength
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container perception">
+              <h3 className="character-sheet__trait-rank">2</h3>
+              <p className="character-sheet__trait-text perception-text">
+                Perception
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container agility">
+              <h3 className="character-sheet__trait-rank">3</h3>
+              <p className="character-sheet__trait-text agility-text">
+                Agility
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container intelligence">
+              <h3 className="character-sheet__trait-rank">3</h3>
+              <p className="character-sheet__trait-text intelligence-text">
+                Intelligence
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container reflexes">
+              <h3 className="character-sheet__trait-rank">2</h3>
+              <p className="character-sheet__trait-text reflexes-text">
+                Reflexes
+              </p>
+            </div>
+            <div className="character-sheet__ring-rank-container awareness">
+              <h3 className="character-sheet__trait-rank">3</h3>
+              <p className="character-sheet__trait-text awareness-text">
+                Awareness
+              </p>
+            </div>
+          </div>
         </div>
         <div className="character-sheet__void-container">
           <p className="character-sheet__void-text">Void Slots</p>
           <div className="character-sheet__void-slots">
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
-            <img
-              src={emptyRingImg}
-              alt=""
-              className="character-sheet__void-slots-image"
-            />
+            <div className="character-sheet__void-slots-image void-spent"></div>
+            <div className="character-sheet__void-slots-image "></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
+            <div className="character-sheet__void-slots-image unavailable"></div>
           </div>
         </div>
-        {/* all rings */}
-        {/* void slots */}
       </section>
 
       <section className="character-sheet__skills-container">
-        <div className="character-sheet__skills-header">
-          <p className="character-sheet__skill-heading">Skill Name</p>
-          <p className="character-sheet__skill-heading">Rank</p>
-          <p className="character-sheet__skill-heading">Trait</p>
-          <p className="character-sheet__skill-heading">Roll</p>
-          <p className="character-sheet__skill-heading">Types</p>
+        <table className="character-sheet__skills-table">
+          <thead>
+            <tr className="character-sheet__skills-header-row">
+              <th className="character-sheet_skills-heading heading-name">
+                Skill Name
+              </th>
+              <th className="character-sheet_skills-heading heading-rank">
+                Rank
+              </th>
+              <th className="character-sheet_skills-heading heading-trait">
+                Trait
+              </th>
+              <th className="character-sheet_skills-heading heading-roll">
+                Roll
+              </th>
+              <th className="character-sheet_skills-heading heading-types">
+                Types
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Athletics
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Strength</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">3k2</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Bugei</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Hunting
+              </td>
+              <td className="character-sheet__skill-rank">2</td>
+              <td className="character-sheet__skill-trait">Perception</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k2</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Bugei</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Kenjutsu
+              </td>
+              <td className="character-sheet__skill-rank">3</td>
+              <td className="character-sheet__skill-trait">Reflexes</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">6k3</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Bugei</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Kyujutsu
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Reflexes</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k2</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Bugei</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Lore: Shadowlands
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Intelligence</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k3</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Low</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Stealth
+              </td>
+              <td className="character-sheet__skill-rank">2</td>
+              <td className="character-sheet__skill-trait">Agility</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">5k3</span>
+              </td>
+              <td className="character-sheet__skill-types">School, Low</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Medicine
+              </td>
+              <td className="character-sheet__skill-rank">3</td>
+              <td className="character-sheet__skill-trait">Intelligence</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">6k3</span>
+              </td>
+              <td className="character-sheet__skill-types">School, High</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Defense
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Reflexes</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">3k2</span>
+              </td>
+              <td className="character-sheet__skill-types">Bugei</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Meditation
+              </td>
+              <td className="character-sheet__skill-rank">2</td>
+              <td className="character-sheet__skill-trait">Void</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k2</span>
+              </td>
+              <td className="character-sheet__skill-types">High</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Investigation
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Perception</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">3k2</span>
+              </td>
+              <td className="character-sheet__skill-types">High, Social</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Courtier
+              </td>
+              <td className="character-sheet__skill-rank">2</td>
+              <td className="character-sheet__skill-trait">Awareness</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">5k3</span>
+              </td>
+              <td className="character-sheet__skill-types">High, Social</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Sincerity
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Awareness</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k3</span>
+              </td>
+              <td className="character-sheet__skill-types">Low, Social</td>
+            </tr>
+
+            <tr className="character-sheet__skills-item">
+              <td className="character-sheet__skill-name pop-text-2">
+                Iaijitsu
+              </td>
+              <td className="character-sheet__skill-rank">1</td>
+              <td className="character-sheet__skill-trait">Agility</td>
+              <td className="character-sheet__skill-roll">
+                <span className="skill-roll">4k3</span>
+              </td>
+              <td className="character-sheet__skill-types">Bugei</td>
+            </tr>
+          </tbody>
+        </table>
+        {/* <div className="character-sheet__skills-header">
+          <p className="character-sheet__skills-heading">Skill Name</p>
+          <p className="character-sheet__skills-heading">Rank</p>
+          <p className="character-sheet__skills-heading">Trait</p>
+          <p className="character-sheet__skills-heading">Roll</p>
+          <p className="character-sheet__skills-heading">Types</p>
         </div>
 
         <div className="character-sheet__skills-item">
@@ -146,7 +351,7 @@ const CharacterSheet = () => {
           <p className="character-sheet__skill-types">School, Bugei</p>
         </div>
         <div className="character-sheet__skills-item">
-          <h4 className="character-sheet__skill-name">Hnting</h4>
+          <h4 className="character-sheet__skill-name">Hunting</h4>
           <p className="character-sheet__skill-rank">2</p>
           <p className="character-sheet__skill-trait">Perception</p>
           <p className="character-sheet__skill-roll">4k2</p>
@@ -186,7 +391,7 @@ const CharacterSheet = () => {
           <p className="character-sheet__skill-trait">Intelligence</p>
           <p className="character-sheet__skill-roll">6k3</p>
           <p className="character-sheet__skill-types">School, High</p>
-        </div>
+        </div> */}
         {/* skill name */}
         {/* rank */}
         {/* trait */}
